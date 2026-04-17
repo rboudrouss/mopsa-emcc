294de31e9b8eac6bb7c630c761bcfef2af64f31a
rboudrouss <rboudrouss@hexaglobe.com>
2026-04-17 14:49:42 +0200

    chore: bad pattern replace

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -1,7 +1,6 @@
 import { Editor } from "@monaco-editor/react";
 import MopsaJs from "../lib/mopsaJs";
 import { handleCustomLanguage, SupportedLanguage } from "../lib";
-import { useEffect, useState } from "react";
 
 export default function CodeEditor({
   language = "universal",
@@ -10,39 +9,20 @@ export default function CodeEditor({
   language?: SupportedLanguage;
   selectedFile: string | null;
 }) {
-  let [file, setFile] = useState<string | null>(selectedFile);
-  let [content, setContent] = useState<string>(getContent());
-
-  useEffect(() => {
-    console.log("Selected file changed", selectedFile);
-
-    setFile(selectedFile);
-    setContent(getContent());
-  }, [selectedFile]);
-
-  function writeFile(content: string) {
-    if (file) {
-      MopsaJs.writeFile(file, content);
-    }
-    MopsaJs.setCode(content);
-  }
-
-  function getContent() {
-    if (file) {
-      return MopsaJs.readFile(file);
-    }
-    return MopsaJs.getCode();
-  }
+  const defaultValue = selectedFile
+    ? MopsaJs.readFile(selectedFile)
+    : MopsaJs.getCode();
 
   return (
     <Editor
+      key={selectedFile ?? "default"}
       height="100%"
       width="100%"
       language={handleCustomLanguage(language)}
-      defaultValue={getContent()}
-      value={content}
-      onChange={(value, _) => {
-        writeFile(value ?? "");
+      defaultValue={defaultValue}
+      onChange={(value) => {
+        if (selectedFile) MopsaJs.writeFile(selectedFile, value ?? "");
+        MopsaJs.setCode(value ?? "");
       }}
     />
   );
