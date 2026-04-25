91c7e1588f18534706483825faa92112201217d0
rboudrouss <r.boudrouss@gmail.com>
2026-04-25 13:57:09 +0200

    chore: removing unused deps & files thanks to knip

--- frontend/src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-// shadcn things
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
